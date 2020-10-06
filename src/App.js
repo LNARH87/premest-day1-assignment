739d2b7f6714146f2a86f186bff000a3e8f0bb51
Leslie Narh <leslienarh@hotmail.com>
2020-10-06 04:31:28 +0000

    redo-component5-features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavComponent from './NavComponent';
 import Links from './Links';
 import About from './About';
 import Species from './Species';
+import Features from './Features';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Links />
       <About />
       <Species />
+      <Features />
     </div>
   );
 }
---
+++ src/Features.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Features() {
+  return (
+    <>  
+        <h3>Features</h3>
+		<p>Brown bears are not always completely brown. Some can be reddish or yellowish.
+        <br></br>They have very large, curved claws and huge paws. Male brown bears are often 30% larger than female brown bears.
+        <br></br>They can range from 5 feet to 9 feet from head to toe</p>
+    </>
+  );
+}
+
+export default Features;

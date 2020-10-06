29bae69772cf1b5697500a8d38e3b21a33fb6532
Leslie Narh <leslienarh@hotmail.com>
2020-10-06 03:45:13 +0000

    added component two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ComponentOne from './ComponentOne';
+import ComponentTwo from './ComponentTwo';
 import './App.css';
 
 function App() {
   return (
     <div>
       <ComponentOne />
+      <ComponentTwo />
     </div>
   );
 }

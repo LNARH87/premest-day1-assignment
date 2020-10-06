a5258d778b65a36c6518f02400aef4f1b49ce7dd
Leslie Narh <leslienarh@hotmail.com>
2020-10-06 04:21:16 +0000

    redo-component2-links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavComponent from './NavComponent';
+import Links from './Links';
 import './App.css';
 
 function App() {
   return (
     <div>
       <NavComponent />
+      <Links />
     </div>
   );
 }
---
+++ src/Links.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Links() {
+  return (
+    <>  
+        <h1>The Brown Bear</h1>
+        <ul>
+                <a href="#introduction"><li>Introduction</li></a>
+                <a href="#habitat"><li>Habitat</li></a>
+                <a href="#media"><li>Media</li></a>
+        </ul>
+    </>
+  );
+}
+
+export default Links;
